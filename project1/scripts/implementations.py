ea1601d16fe65bac57533ad69f9c048d09499f38
ch-epfl-262980 <ch-epfl-262980@noto.epfl.ch>
2020-09-29 08:39:19 +0000

    Add implementation.py file

---
+++ project1/scripts/implementations.py
@@ -0,0 +1,23 @@
+#Implementation functions
+#all functions return (w, loss)
+
+import numpy as np
+
+
+def least_squares GD(y, tx, initial w, max iters, gamma):
+    #Linear regression using gradient descent
+
+def least_squares SGD(y, tx, initial w, max iters, gamma):
+    #Linear regression using stochastic gradient descent
+
+def least_squares(y, tx):
+    #Least squares regression using normal equations
+
+def ridge_regression(y, tx, lambda_):
+    #Ridge regression using normal equations
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    #Logistic regression using gradient descent or SGD
+
+def reg_logistic_regressions(y, tx, lambda_, initial_w, max_iters, gamma):
+    #Regularized logistic regression using gradient descent or SGD
