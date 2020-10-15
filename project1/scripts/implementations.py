6c1c79b92e7d002fc2e263386c64d99bb36e9b6d
ch-epfl-287052 <ch-epfl-287052@noto.epfl.ch>
2020-10-15 17:51:20 +0000

    2 new notebooks

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -70,29 +70,69 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
-    #Ridge regression using normal equations
+    '''Ridge regression using normal equations'''
     I = np.identity(tx.shape[1])             #I(dxd)
     l = 2*lambda_*tx.shape[0]                #lambda' = 2*lambda*N
     
     A = tx.T@tx + l*I
     B = tx.T@y
     w_ridge = np.linalg.solve(A,B)
-    
     #calculate loss with mse
     err = y-tx.dot(w_ridge)
-    mse = np.mean(err**2)/2
-    
+    mse = compute_loss_MSE(y, tx, w_ridge)
     return w_ridge, mse
+
+
+def sigmoid(t):
+    """apply the sigmoid function on t."""
+    e_t = np.exp(t)
+    return 1/(1+1/e_t)
+
+def calculate_gradient(y, tx, w):
+    """compute the gradient of loss."""
+    return tx.T@(sigmoid(tx@w)-y)
+
+def calculate_loss(y, tx, w):
+    """compute the loss: negative log likelihood."""
+    sig = sigmoid(np.dot(tx,w))
+    return -(y.T.dot(np.log(sig))+(1-y).T.dot(np.log(1-sig)))
+
+def calculate_hessian(y, tx, w):
+    """return the Hessian of the loss function."""
+    one = np.ones(tx.shape[0]).T
+    S = np.diag(np.diag(sigmoid(tx@w)*(one-sigmoid(tx@w))))
+    return tx.T@S@tx
 
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """Logistic regression using gradient descent or SGD"""
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        gradient = calculate_gradient(y, tx, w)
+        w = w - gamma * gradient
+        loss =  calculate_loss(y, tx, w)
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+    return ws[-1],losses[-1]
+
 
 def reg_logistic_regressions(y, tx, lambda_, initial_w, max_iters, gamma):
     """Regularized logistic regression using gradient descent or SGD"""
-
-    
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        gradient = calculate_gradient(y, tx, w)+2*lambda_*np.linalg.norm(w)
+        w = w - gamma * gradient
+        loss =  calculate_loss(y, tx, w)+ lambda_*np.linalg.norm(w)
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+    return ws[-1],losses[-1]
     
 #----------------------------------------------------------
 #Feature processing
@@ -100,7 +140,7 @@ def reg_logistic_regressions(y, tx, lambda_, initial_w, max_iters, gamma):
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
     
-    phi = np.zeros((x.shape[0], 1)) #degree + 1 because we want to go from 0 to degree
+    phi = np.ones((x.shape[0], 1)) #degree + 1 because we want to go from 0 to degree
     for d in range(degree+1):   
          phi = np.c_[phi, x**d]
     return phi
@@ -124,7 +164,7 @@ def split_data(x, y, ratio, seed=1):
 #----------------------------------------------------------
 # Cross Validation
 
-def cross_validation(y, x, k_indices, k, lambda_, degree):
+def cross_validation(y, x, k_indices, k, initial_w, lambda_, max_iters, gamma, degree):
     """return the loss of ridge regression."""
     # get k'th subgroup in test, others in train
     test_idx = k_indices[k]
@@ -139,7 +179,8 @@ def cross_validation(y, x, k_indices, k, lambda_, degree):
     y_te = y[test_idx]
 
     # ridge regression
-    w_r, mse = ridge_regression(y_tr, x_tr, lambda_)
+    initial_w = np.zeros(x_tr.shape[1])
+    w_r, mse = reg_logistic_regressions(y_tr, x_tr, lambda_, initial_w, max_iters, gamma)
 
     # calculate the loss for train and test data
     loss_tr = compute_loss_MSE(y_tr, x_tr, w_r)
